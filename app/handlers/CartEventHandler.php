d91a4785bf469c03f6b169b1add578255cbf6013
Bruno Gaspar <brunofgaspar1@gmail.com>
2014-08-27 05:23:47 +0100

    small refactor on how events are handled
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ app/handlers/CartEventHandler.php
@@ -0,0 +1,160 @@
+<?php namespace App\Handlers;
+
+use App\Models\Product;
+use Cartalyst\Cart\Cart;
+use Illuminate\Events\Dispatcher;
+use Cartalyst\Cart\Collections\ItemCollection;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+
+class CartEventHandler {
+
+	/**
+	 * The logged in user instance.
+	 *
+	 * @var \Cartalyst\Sentinel\Sentinel\Users\EloquentUser
+	 */
+	protected $user = null;
+
+	/**
+	 * Constructor.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->user = Sentinel::getUser();
+	}
+
+	/**
+	 * When an item is added to the cart.
+	 *
+	 * @param  \Cartalyst\Cart\Collections\ItemCollection  $item
+	 * @param  \Cartalyst\Cart\Cart  $cart
+	 * @return void
+	 */
+	public function onItemAdded(ItemCollection $item, Cart $cart)
+	{
+		// Check if the user is logged in
+		if ( ! $this->user) return;
+
+		// Store the item on the database
+		$this->storeItem($item, $cart);
+	}
+
+	/**
+	 * When an item from the cart is updated.
+	 *
+	 * @param  \Cartalyst\Cart\Collections\ItemCollection  $item
+	 * @param  \Cartalyst\Cart\Cart  $cart
+	 * @return void
+	 */
+	public function onItemUpdated(ItemCollection $item, Cart $cart)
+	{
+		// Check if the user is logged in
+		if ( ! $this->user) return;
+
+		// Update the item on the database
+		$this->storeItem($item, $cart);
+	}
+
+	/**
+	 * When an item is removed from the cart.
+	 *
+	 * @param  \Cartalyst\Cart\Collections\ItemCollection  $item
+	 * @param  \Cartalyst\Cart\Cart  $cart
+	 * @return void
+	 */
+	public function onItemRemoved(ItemCollection $item, Cart $cart)
+	{
+		// Check if the user is logged in
+		if ( ! $this->user) return;
+
+		// Get the product that was added to the shopping cart
+		$product = Product::find($item->get('id'));
+
+		// Remove the product from the database
+		$this->findCart($cart->getIdentity())->items()->whereProductId($product->id)->delete();
+	}
+
+	/**
+	 * When the cart is cleared.
+	 *
+	 * @param  \Cartalyst\Cart\Cart  $cart
+	 * @return void
+	 */
+	public function onCartCleared(Cart $cart)
+	{
+		// Check if the user is logged in
+		if ( ! $this->user) return;
+
+		// Remove all the items from the database
+		$this->findCart($cart->getIdentity())->items()->delete();
+	}
+
+	/**
+	 * Listen to the events.
+	 *
+	 * @param  Illuminate\Events\Dispatcher  $dispatcher
+	 * @return void
+	 */
+	public function subscribe(Dispatcher $dispatcher)
+	{
+		$dispatcher->listen('cartalyst.cart.added', 'App\Handlers\CartEventHandler@onItemAdded');
+
+		$dispatcher->listen('cartalyst.cart.updated', 'App\Handlers\CartEventHandler@onItemUpdated');
+
+		$dispatcher->listen('cartalyst.cart.removed', 'App\Handlers\CartEventHandler@onItemRemoved');
+
+		$dispatcher->listen('cartalyst.cart.cleared', 'App\Handlers\CartEventHandler@onCartCleared');
+	}
+
+	/**
+	 * Returns the user cart associated to the given cart instance.
+	 *
+	 * @param  string  $instance
+	 * @return \App\Models\Cart
+	 */
+	protected function findCart($instance)
+	{
+		if ( ! $cart = $this->user->cart()->whereInstance($instance)->first())
+		{
+			$cart = $this->user->cart()->create(compact('instance'));
+		}
+
+		return $cart;
+	}
+
+	/**
+	 * Store or update the item on local storage.
+	 *
+	 * @param  \Cartalyst\Cart\Collections\ItemCollection  $item
+	 * @param  \Cartalyst\Cart\Cart  $cart
+	 * @return \App\Models\CartItem
+	 */
+	protected function storeItem(ItemCollection $item, Cart $cart)
+	{
+		// Get the product that was added to the shopping cart
+		$product = Product::find($item->get('id'));
+
+		// Get the cart from storage that belongs to the instance
+		$_cart = $this->findCart($cart->getIdentity());
+
+		// Does the product exist on storage?
+		if ( ! $_item = $_cart->items()->whereProductId($product->id)->first())
+		{
+			$_item = $_cart->items()->create([
+				'product_id' => $product->id,
+				'quantity'   => $item->get('quantity'),
+			]);
+		}
+		else
+		{
+			$_item->update([
+				'quantity' => $item->get('quantity')
+			]);
+		}
+
+		return $_item;
+	}
+
+}
---
+++ app/handlers/UserEventHandler.php
@@ -0,0 +1,101 @@
+<?php namespace App\Handlers;
+
+use Illuminate\Events\Dispatcher;
+use Illuminate\Support\Collection;
+use Cartalyst\Sentinel\Users\EloquentUser;
+use Cartalyst\Sentinel\Laravel\Facades\Sentinel;
+
+class UserEventHandler {
+
+	/**
+	 * The logged in user instance.
+	 *
+	 * @var \Cartalyst\Sentinel\Sentinel\Users\EloquentUser
+	 */
+	protected $user = null;
+
+	/**
+	 * Constructor.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		$this->user = Sentinel::getUser();
+	}
+
+	/**
+	 * When a user logs in.
+	 *
+	 * @param  \Cartalyst\Sentinel\Users\EloquentUser  $user
+	 * @return void
+	 */
+	public function onUserAuthenticated(EloquentUser $user)
+	{
+		// Get the main cart instance
+		$cart = app('cart');
+
+		// Get the wishlist cart instance
+		$wishlist = app('wishlist');
+
+		$items = [];
+
+		foreach ($user->cart as $_cart)
+		{
+			foreach ($_cart->items as $item)
+			{
+				$id = $item->product->id;
+
+				$search = $cart->find(compact('id'));
+
+				if (count($search) === 0)
+				{
+					$items[$_cart->instance][] = [
+						'id'       => $id,
+						'name'     => $item->product->name,
+						'price'    => $item->product->price,
+						'quantity' => 1,
+					];
+				}
+			}
+		}
+
+		$instance = $cart->getIdentity();
+
+		if ( ! $_cart = $this->user->cart()->whereInstance($instance)->first())
+		{
+			$_cart = $this->user->cart()->create(compact('instance'));
+		}
+
+		foreach ($_cart->items() as $item)
+		{
+			$id = $item->get('id');
+
+			if ( ! $_cart->items()->where('product_id', $id)->first())
+			{
+				$_cart->items()->create([
+					'product_id' => $id,
+					'quantity'   => $item->get('quantity'),
+				]);
+			}
+		}
+
+		// Sync the main Cart
+		$cart->sync(new Collection(array_get($items, 'main', [])));
+
+		// Sync the wishlist
+		$wishlist->sync(new Collection(array_get($items, 'wishlist', [])));
+	}
+
+	/**
+	 * Listen to the events.
+	 *
+	 * @param  Illuminate\Events\Dispatcher  $dispatcher
+	 * @return void
+	 */
+	public function subscribe(Dispatcher $dispatcher)
+	{
+		$dispatcher->listen('sentinel.authenticated', 'App\Handlers\UserEventHandler@onUserAuthenticated');
+	}
+
+}
